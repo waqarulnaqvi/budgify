dd4e4b9486cfc1b7433b0d231c282de2886385d9
Syed Waqarul Hasan Naqvi <syedwaqarul786@gmail.com>
2025-12-21 20:55:24 +0530

    - connected with firebase - adding the permission handler - added all the required things for the alarm

--- lib/main.dart
+++ lib/main.dart
@@ -3,82 +3,85 @@ import 'package:budgify/shared/viewmodel/riverpod/theme_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'core/constants/prefs_keys.dart';
 import 'core/local/prefs_helper.dart';
 import 'core/routes/app_routes.dart';
 import 'core/routes/paths.dart';
 import 'core/theme/app_theme.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // await _loadFonts();
+  await MobileAds.instance.initialize(); // ✅ Initialize AdMob SDK
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await _loadFonts();
 
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    statusBarIconBrightness: Brightness.dark,
-    statusBarBrightness: Brightness.dark,
-    // systemNavigationBarColor: AppColors.themeLight,
-  ));
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark,
+      // systemNavigationBarColor: AppColors.themeLight,
+    ),
+  );
   PrefsHelper prefs = PrefsHelper();
   final bool isSeenOnBoard =
       await prefs.getBoolValue(PrefsKeys.isSeenOnBoard) ?? false;
 
-  runApp(ProviderScope(child: MyApp(isSeenOnBoard: isSeenOnBoard),));
+  runApp(ProviderScope(child: MyApp(isSeenOnBoard: isSeenOnBoard)));
 }
 
-// Future<void> _loadFonts() async {
-//   // Load Poppins font
-//   final fontLoaderPoppins = FontLoader('Poppins')
-//     ..addFont(rootBundle.load('assets/fonts/Poppins-Regular.ttf'));
-//
-//   // Load Montserrat font
-//   final fontLoaderMontserrat = FontLoader('Montserrat')
-//     ..addFont(rootBundle.load('assets/fonts/Montserrat-Regular.ttf'));
-//
-//   // Load both fonts
-//   await Future.wait([
-//     fontLoaderPoppins.load(),
-//     fontLoaderMontserrat.load(),
-//   ]);
-// }
+
+Future<void> _loadFonts() async {
+  // Load Poppins font
+  final fontLoaderPoppins = FontLoader('Poppins')
+    ..addFont(rootBundle.load('assets/fonts/Poppins-Regular.ttf'));
+
+  // Load Montserrat font
+  final fontLoaderMontserrat = FontLoader('Montserrat')
+    ..addFont(rootBundle.load('assets/fonts/Montserrat-Regular.ttf'));
+
+  // Load both fonts
+  await Future.wait([
+    fontLoaderPoppins.load(),
+    fontLoaderMontserrat.load(),
+  ]);
+}
 
 class MyApp extends ConsumerWidget {
   final bool isSeenOnBoard;
 
   const MyApp({super.key, required this.isSeenOnBoard});
 
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var themeAsyncValue = ref.watch(isLightThemeProvider);
-    // TextTheme textTheme = createTextTheme(
-    //   context,
-      // "Noto Music",
-      // "Noto Sans Display",
-    // );
-    MaterialTheme theme = MaterialTheme(
-        // textTheme
-    );
+
+    MaterialTheme theme = MaterialTheme();
 
     return MaterialApp(
       title: Constants.budgetFlow,
       debugShowCheckedModeBanner: false,
       theme: theme.light(),
       darkTheme: theme.dark(),
-      // themeMode: ThemeMode.system,
-      // theme: themeAsyncValue.when(
-      //   data: (isLight) => isLight ? theme.light() : theme.dark(),
-      //   loading: () => theme.light(), // default while loading
-      //   error: (_, __) => theme.light(),
-      // ),
+
       themeMode: themeAsyncValue.when(
         data: (isLight) => isLight ? ThemeMode.light : ThemeMode.dark,
         loading: () => ThemeMode.light, // default while loading
         error: (_, __) => ThemeMode.light,
       ),
       onGenerateRoute: AppRoutes.onGenerateRoute,
-      initialRoute: isSeenOnBoard ?  Paths
-          .initial : Paths.onboardingPage,
+      initialRoute: isSeenOnBoard ? Paths.initial : Paths.onboardingPage,
+      navigatorObservers: [FirebaseAnalyticsObserver(analytics:
+      analytics)],
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,11 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
 import flutter_inappwebview_macos
+import flutter_local_notifications
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
@@ -14,7 +18,11 @@ import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
